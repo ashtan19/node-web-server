4be77c7cb1f6a06aa81620610fdd14353ece0ff9
Ash Tan <ashtan19@gmail.com>
2018-06-26 20:28:41 -0700

    Projects Page Addition

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get("/about", (req, res) => {
     });
 });
 
+app.get("/projects", (req, res) => {
+    res.render("projects.hbs", {
+        pageTitle: "Projects Page",
+        projectsMessage: "Maple Projects are here!"
+    });
+});
+
 app.get("/bad", (req,res) => {
     res.send({
         errorMessage: "Unable to fulfill request"
